f7f1c3e2b06615d6e6d3ed0c11ad9626c9433b2b
Anastasia <145789295+ArbakovaAnastasia@users.noreply.github.com>
2023-11-29 14:04:45 +0800

    Add files via upload

--- carDetectionNumber/main.py
+++ carDetectionNumber/main.py
@@ -1,15 +1,16 @@
 #pip install matplotlib pytesseract opencv-python
 import matplotlib.pyplot as plt
-import pytesseract
+import pytesseract as pt
 import cv2
 
-#получаем изображение
 def open_img(img_path):
     carplate_img = cv2.imread(img_path)
     carplate_img = cv2.cvtColor(carplate_img, cv2.COLOR_BGR2RGB)
     plt.axis('off')
     plt.imshow(carplate_img)
-    plt.show()
+    #plt.show()
+    plt.savefig('carplate_img.png')  # Save the image
+    plt.close()
 
     return carplate_img
 
@@ -31,12 +32,51 @@ def enlarge_img(image, scale_percent):
     return resized_image
 
 def main():
-    carplate_img_rgb = open_img(img_path = 'C:\\Users\\Nastya\\Desktop\\carDetectionNumber\\cars\\4.jpg')
+    carplate_img_rgb = open_img(img_path = 'C:\\Users\\Nastya\\Desktop\\carDetectionNumber\\cars\\3.jpg')
     carplate_haar_cascade = cv2.CascadeClassifier('C:\\Users\\Nastya\\Desktop\\carDetectionNumber\\haar_cascades\\haarcascade_russian_plate_number.xml')
     carplate_extract_img = carplate_extract(carplate_img_rgb, carplate_haar_cascade)
     carplate_extract_img = enlarge_img(carplate_extract_img, 150)
     plt.imshow(carplate_extract_img)
-    plt.show()
+    #plt.show()
+    plt.savefig('carplate_extract_img.png')  # Save the extracted carplate image
+    plt.close()
+
+    carplate_extract_img_gray = cv2.cvtColor(carplate_extract_img, cv2.COLOR_RGB2GRAY)
+    plt.axis('off')
+    plt.imshow(carplate_extract_img_gray, cmap='gray')
+    #plt.show()
+    plt.savefig('carplate_extract_img_gray.png')  # Save the grayscale carplate image
+    plt.close()
+
+    pt.pytesseract.tesseract_cmd = 'C:\\Users\\Nastya\\AppData\\Local\\Tesseract.exe'
+    print('Номер автомобиля:  ', pt.image_to_string(
+        carplate_extract_img_gray,
+        config='--psm 6 --oem 3 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+    )
+
+    # Generate HTML file with the results
+    with open('carplate_detection_result.html', 'w') as file:
+        file.write('''
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Car Detection Result</title>
+    </head>
+    <body>
+        <h1>Car Detection Result</h1>
+        <div><p>Number of car: {carplate_number}</p></div>
+        Picture:
+        <div><img src="carplate_img.png" alt="Carplate Image">
+        <img src="carplate_extract_img.png" alt="Extracted Carplate Image"></div>
+        <!-- <div><img src="carplate_extract_img_gray.png" alt="Grayscale Carplate Image"></div> -->
+        
+    </body>
+    </html>
+    '''.format(carplate_number=pt.image_to_string(
+        carplate_extract_img_gray,
+        config='--psm 6 --oem 3 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    )))
+
 
 if __name__ == '__main__':
     main()
